84f683998cb96ac1824ebdef23ac2afaaae6c825
TravisCeyler <26.Travis.Ceyler@sidneycityschools.org>
2025-11-11 07:53:51 -0500

    added new scripts button scriptsr

---
+++ Assets/Scripts/CutscenceTrigger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Playables;
+//using StarterAssets; // if you're using Starter Assets package
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public GameObject player;
+    //private ThirdPersonController controller;
+
+    void Start()
+    {
+       // controller = player.GetComponent<ThirdPersonController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           // controller.enabled = false; // disable movement
+            timeline.Play();
+            timeline.stopped += OnCutsceneEnd;
+        }
+    }
+
+    private void OnCutsceneEnd(PlayableDirector director)
+    {
+       // controller.enabled = true; // re-enable player control
+    }
+}
